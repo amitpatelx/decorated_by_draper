870e8914c924ab8abdc582e7506df872111bcba9
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2014-08-07 11:24:20 +0530

    First commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.17'
+
+gem 'pg'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'draper', '~> 1.3.1'  #Decorators/View-Models for Rails Applications
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DecoratedByDraper::Application.config.secret_token = '37beeaa6114910676967769ee198c6c2880d7123c5afec59d11231915a1b7fa2210a0293284a7da375f7c687d329c057945d447d3ff9eb3b6eeee6be7f8d0929'
